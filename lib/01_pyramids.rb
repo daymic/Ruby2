384a8570047a63e492ea153ea2f0634fe42ef9ff
daymic <hernafidy@gmail.com>
2019-01-11 08:55:23 +0100

    final

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,3 +1,16 @@
+def half_pyramid
+	puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+	print ">"
+	n = gets.chomp.to_i
+	puts "Voici la pyramide :"
+	for i in 1..n
+		x = '#'*i
+		y = ' '*(n-i)
+		puts  "#{x}#{y}"
+	end
+end
+half_pyramid
+
 def full_pyramid
 puts "entre le nombre de ligne du pyramide"
 i=1
@@ -6,18 +19,46 @@ n= gets.chomp.to_i
 while i<=n do
 	b = n-i
 	c=i-1
-puts (" " *b+"#" *i + "#"*c)
-i=i+1
+		puts (" " *b+"#" *i + "#"*c)
+		i=i+1
 
+			end
+			end
+		 		def greet (full_pyramid)
+			 	puts full_pyramid.reverse
+	 			end
+
+			def perform
+	greet(full_pyramid)
 end
-end
- def greet (full_pyramid)
- 	puts full_pyramid.reverse
- end
+full_pyramid
 
-def perform
-greet(full_pyramid)
-end
 
-perform
+def wtf_pyramid
+  
+    puts "Salut, bienvenue au full PYRAMIDE ! Combien d'étages veux-tu ? "
+    print "-->"
 
+  nombre=gets.chomp.to_i
+  while nombre.even?
+    puts "Nombre impaire svp "
+    print "-->"
+    nombre = gets.chomp.to_i
+  end
+  nb = nombre/2
+  g=0
+  for i in (1..nb)
+    print " " * nb
+    puts "#" * (i+g)
+    g+=1
+    nb-=1
+  end
+  l=i+1
+  k=0
+  for nb in (nb..i)
+    print " " * nb
+    puts "#" * (i+l-k)
+    k+=2
+  end
+  end
+  wtf_pyramid
